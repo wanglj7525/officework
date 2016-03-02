94f95594ada0394b8eb9631cf25f4b6ee6f0f845
Mr-try <try418@qq.com>
2016-03-02 17:28:44 +0800

    调配原因

--- public/app/js/controllers/adjust.js
+++ public/app/js/controllers/adjust.js
@@ -1,7 +1,35 @@
 'use strict';
-app.controller('adjustdetailController',[ '$scope', '$http', '$state','$timeout','$stateParams','adjustdetailservice',
-	function($scope, $http, $state, $timeout,$stateParams,adjustdetailservice) {
+app.controller('analysisReasonController', ['$scope', '$modalInstance', 'items',function($scope, $modalInstance,items) {
+		$scope.ok = function () {
+			$scope.reasontitle= $("#reasontitle").val();
+			$scope.reasondetail= $("#reasondetail").val();
+			$modalInstance.close($scope.reasontitle,$scope.reasondetail);
+	};
+
+	$scope.cancel = function () {
+		$modalInstance.dismiss('cancel');
+	};
+}]);
+app.controller('adjustdetailController',[ '$scope', '$http', '$state','$timeout','$stateParams','$modal','$log','adjustdetailservice',
+	function($scope, $http, $state, $timeout,$stateParams,$modal,$log,adjustdetailservice) {
 		$scope.id = $stateParams.id;
+		$scope.addReason= function () {
+				var modalReasonInstance = $modal.open({
+					templateUrl: 'adjustReasonModel.html',
+					controller: 'analysisReasonController',
+					size: 'md',
+					resolve: {
+						items: function () {
+							return $scope.reasondetail;
+						}
+					}
+				});
+				modalReasonInstance.result.then(function (reasonTitleValue,reasonDetailValue) {
+					$scope.reasons.push({"id":1,"title":reasonTitleValue,"detail":reasonDetailValue});
+				}, function () {
+					$log.info('Modal dismissed at: ' + new Date());
+				});
+			}
 		adjustdetailservice.getData().then(
 			function (res) {
 				$scope.adjusttable = res.data.info
@@ -23,6 +51,7 @@ app.controller('adjustController',[ '$scope', '$http', '$state','$timeout','adju
 						console.log(rej);
 					}
 				);
+				
 				//获取人员信息
 				adjustlistservice.getData().then(
 					function (res) {
@@ -32,23 +61,7 @@ app.controller('adjustController',[ '$scope', '$http', '$state','$timeout','adju
 						console.log(rej);
 					}
 				);
-			  
-
-	//$scope.selecttable = [
-	//               { id:1,img:'/public/app/img/a0.jpg', name:'张三1' ,sex:"男",company:"福清市xxx、xxx信息1",nation:"汉",birthday:"196511",palce:"福清龙田",troops:"197712",party:"191212",education:"本科",school:"福建师范"},
-	//               { id:2,img:'/public/app/img/a1.jpg', name:'张三2' ,sex:"男",company:"福清市xxx、xxx信息2",nation:"汉",birthday:"195411",palce:"福清龙田",troops:"197212",party:"197712",education:"本科",school:"福建师范"},
-	//               { id:3,img:'/public/app/img/a2.jpg', name:'张三3' ,sex:"男",company:"福清市xxx、xxx信息3",nation:"汉",birthday:"195711",palce:"福清龙田",troops:"191212",party:"197712",education:"本科",school:"福建师范"},
-	//               { id:4,img:'/public/app/img/a3.jpg', name:'张三4' ,sex:"女",company:"福清市xxx、xxx信息4",nation:"汉",birthday:"195311",palce:"福清龙田",troops:"197212",party:"197212",education:"本科",school:"福建师范"},
-	//               { id:5,img:'/public/app/img/a4.jpg', name:'张三5' ,sex:"男",company:"福清市xxx、xxx信息5",nation:"汉",birthday:"195511",palce:"福清龙田",troops:"191212",party:"197712",education:"本科",school:"福建师范"},
-	//               { id:6,img:'/public/app/img/a5.jpg', name:'张三6' ,sex:"男",company:"福清市xxx、xxx信息6",nation:"汉",birthday:"195011",palce:"福清龙田",troops:"197212",party:"197712",education:"本科",school:"福建师范"},
-	//               { id:7,img:'/public/app/img/a6.jpg', name:'张三7' ,sex:"男",company:"福清市xxx、xxx信息7",nation:"汉",birthday:"197511",palce:"福清龙田",troops:"197712",party:"197212",education:"本科",school:"福建师范"},
-	//               { id:8,img:'/public/app/img/a7.jpg', name:'张三8' ,sex:"男",company:"福清市xxx、xxx信息8",nation:"汉",birthday:"198511",palce:"福清龙田",troops:"191212",party:"171212",education:"本科",school:"福建师范"},
-	//               { id:9,img:'/public/app/img/a8.jpg', name:'张三9' ,sex:"男",company:"福清市xxx、xxx信息",nation:"汉",birthday:"195411",palce:"福清龙田",troops:"197712",party:"197712",education:"本科",school:"福建师范"},
-	//               { id:10,img:'/public/app/img/a9.jpg', name:'张三10',sex:"男",company:"福清市xxx、xxx信息",nation:"汉",birthday:"195511",palce:"福清龙田",troops:"197212",party:"191212",education:"本科",school:"福建师范" },
-	//               { id:11,img:'/public/app/img/a0.jpg', name:'张三1' ,sex:"男",company:"福清市xxx、xxx信息",nation:"汉",birthday:"195511",palce:"福清龙田",troops:"197212",party:"197712",education:"本科",school:"福建师范"},
-	//               { id:12,img:'/public/app/img/a1.jpg', name:'张三2' ,sex:"女",company:"福清市xxx、xxx信息",nation:"汉",birthday:"195511",palce:"福清龙田",troops:"197712",party:"197212",education:"本科",school:"福建师范"},
-	//               {id:21,img:'/public/app/img/a10.jpg', name:'张三11' ,sex:"男",company:"福清市xxx、xxx信息",nation:"汉",birthday:"195111",palce:"福清龙田",troops:"191212",party:"197712",education:"本科",school:"福建师范"}];
-	
+								
 	 $scope.userState = '';
 
 	  $scope.toppings = [
@@ -58,16 +71,6 @@ app.controller('adjustController',[ '$scope', '$http', '$state','$timeout','adju
 	                     {name:'科员',id:'4'},
 	                     {name:'领导',id:'5'},
 	                     {name:'退休',id:'6'}
-//	    { category: 'meat', name: 'Pepperoni' },
-//	    { category: 'meat', name: 'Sausage' },
-//	    { category: 'meat', name: 'Ground Beef' },
-//	    { category: 'meat', name: 'Bacon' },
-//	    { category: 'veg', name: 'Mushrooms' },
-//	    { category: 'veg', name: 'Onion' },
-//	    { category: 'veg', name: 'Green Pepper' },
-//	    { category: 'veg', name: 'Green Olives' },
 	  ];
-
-	
 } ]);
 

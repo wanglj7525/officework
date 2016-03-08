b9d5e874537a7a67ddfc7487f6571d8d04954d39
wanglj <wanglj@int-yt.com>
2016-03-08 17:38:44 +0800

    代码添加修改删除添加接口

--- public/app/js/controllers/setting_daima.js
+++ public/app/js/controllers/setting_daima.js
@@ -1,18 +1,174 @@
 'use strict';
-app.controller('SetDaimaCtrl',['$rootScope','$state','$scope','daimaservice',function($rootScope,$state,$scope,daimaservice){
+app.controller('SetDaimaCtrl',['$rootScope','$state','$scope','$modal','$log','$localStorage','SettingdaimaService',function($rootScope,$state,$scope,$modal,$log,$localStorage,SettingdaimaService){
 
 //获取人员信息
-    daimaservice.getData().then(
+    SettingdaimaService.getCatalogList().then(
         function (res) {
             $scope.daimalist = res.data.info;
-            $scope.daima= $scope.daimalist[0];
+            $scope.currentname=$scope.daimalist[0].name;
+            SettingdaimaService.getCodagetList($scope.daimalist[0].name).then(
+                function(res){
+                    $scope.daima=res.data.info;
+                },
+                function(rej){
+                    console.log(rej);
+                }
+            );
         },
         function (rej) {
             console.log(rej);
         }
     );
 
+    //选择代码目录
     $scope.selectdaima=function(daima){
-        $scope.daima=daima;
+        $scope.currentname=daima.name;
+        SettingdaimaService.getCodagetList(daima.name).then(
+            function(res){
+                $scope.daima=res.data.info;
+            },
+            function(rej){
+                console.log(rej);
+            }
+        );
+
     }
+
+    //添加代码
+    $scope.addDaima=function(catalog_id){
+        console.log(catalog_id);
+        var modaldaimaInstance = $modal.open({
+            templateUrl: 'saveAddDaimaModel.html',
+            controller: 'ModalAddDaimaInstanceCtrl',
+            size: 'md'
+        });
+        modaldaimaInstance.result.then(function (newdata) {
+            var params=$.param({
+                catalog_id:  catalog_id,
+                ano: newdata.ano,
+                dz:  newdata.dz,
+                jc:  newdata.jc,
+                access_token:$localStorage.token
+            });
+            //调用后台保存 成功后修改页面
+            SettingdaimaService.addDaima(params).then(
+                function (res) {
+                    console.log(res);
+                    if(res.data.code==200){
+                        $scope.daima.list.push(res.data.info);
+                    }else{
+                        alert(res.data.msg);
+                    }
+
+                },
+                function (rej) {
+                    console.log(rej);
+                }
+            );
+        }, function () {
+            $log.info('Modal dismissed at: ' + new Date());
+        });
+    }
+    //删除代码
+    $scope.deletedaima=function(idx,data,catalog_id){
+        var modaldaimadeleteInstance = $modal.open({
+            templateUrl: 'deleteDaimaModel.html',
+            controller: 'ModalDeleteDaimaInstanceCtrl',
+            size: 'sm',
+            resolve: {
+                data: function () {
+                    return data;
+                }
+            }
+        });
+        modaldaimadeleteInstance.result.then(function () {
+            var params=$.param({
+                catalog_id: catalog_id,
+                ano:data.ano,
+                access_token:$localStorage.token
+            });
+            SettingdaimaService.deleteDaima(params).then(
+                function (res) {
+                    if(res.data.code==200){
+                        $scope.daima.list.splice(idx,1);
+                    }else{
+                        alert(res.data.msg);
+                    }
+                },
+                function (rej) {
+                    console.log(rej);
+                }
+            );
+        }, function () {
+            $log.info('Modal dismissed at: ' + new Date());
+        });
+    }
+    //修改代码
+    $scope.updatedaima=function(catalog_id,data){
+        $scope.newdata=angular.copy(data);
+        var modalupdatezwInstance = $modal.open({
+            templateUrl: 'saveUpdateDaimaModel.html',
+            controller: 'ModalUpdateDaimaInstanceCtrl',
+            size: 'md',
+            resolve: {
+                newdata: function () {
+                    return  $scope.newdata;
+                }
+            }
+        });
+        modalupdatezwInstance.result.then(function () {
+            var params=$.param({
+                catalog_id:  catalog_id,
+                ano: $scope.newdata.ano,
+                dz:  $scope.newdata.dz,
+                jc:  $scope.newdata.jc,
+                access_token:$localStorage.token
+            });
+            //调用后台保存 成功后修改页面
+            SettingdaimaService.updateDaima(params).then(
+                function (res) {
+                    if(res.data.code==200){
+                        angular.copy($scope.newdata,data);
+                    }else{
+                        alert(res.data.msg);
+                    }
+                },
+                function (rej) {
+                    console.log(rej);
+                }
+            );
+        }, function () {
+            $log.info('Modal dismissed at: ' + new Date());
+        });
+    }
+}]);
+app.controller('ModalAddDaimaInstanceCtrl', ['$scope', '$modalInstance',function($scope, $modalInstance) {
+    $scope.newdata={};
+    $scope.ok = function () {
+        $modalInstance.close($scope.newdata);
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+}]);
+app.controller('ModalDeleteDaimaInstanceCtrl', ['$scope', '$modalInstance', 'data',function($scope, $modalInstance,data) {
+    $scope.data = data;
+    $scope.ok = function () {
+        $modalInstance.close();
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+}]);
+app.controller('ModalUpdateDaimaInstanceCtrl', ['$scope', '$modalInstance', 'newdata',function($scope, $modalInstance,newdata) {
+    $scope.newdata = newdata;
+    $scope.ok = function () {
+        $modalInstance.close();
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
 }]);
--- public/app/js/services/httpservice.js
+++ public/app/js/services/httpservice.js
@@ -1,102 +1,168 @@
 angular.module('app')
-    .service('SeetingtreeService',['$q','$http','SERVICE_URL','$localStorage',function($q,$http,SERVICE_URL,$localStorage){
-        return{
-            getTreeList:function(id){
-                var deferred=$q.defer();
-                var path=SERVICE_URL+'/setting/tree/getlist?parent=0&access_token='+$localStorage.token;
-                if (id) {
-                    path+='';
-                };
-                var promise=$http.get(path).then(function(response){
-                    console.log(response);
-                    return response;
-                },function(response){
-                    console.log(response);
-                    return response;
-                });
-                return promise;
-            },
-            addTree:function(params){
-                var deferred=$q.defer();
-                var path=SERVICE_URL+'/setting/tree/add';
-                var promise=$http.post(path, params).then(function(response){
-                    return response;
-                },function(response){
-                    console.log(response);
-                    return response;
-                });
-                return promise;
-            },
-            updateTree:function(params){
-                var deferred=$q.defer();
-                var path=SERVICE_URL+'/setting/tree/update?';
-                var promise=$http.put( path+ params).then(function(response){
-                    return response;
-                },function(response){
-                    console.log(response);
-                    return response;
-                });
-                return promise;
-            },
-            deleteTree:function(params){
-                var deferred=$q.defer();
-                var path=SERVICE_URL+'/setting/tree/delete?';
-                var promise=$http.delete( path+ params).then(function(response){
-                    return response;
-                },function(response){
-                    console.log(response);
-                    return response;
-                });
-                return promise;
-            },
-            getPositionList:function(id){
-                var deferred=$q.defer();
-                var params=$.param({
-                    tree_id:  id,
-                    access_token:  $localStorage.token
-                });
-                var path=SERVICE_URL+'/setting/tree/position/getlist?';
-                var promise=$http.get(path+params).then(function(response){
-                    console.log(response);
-                    return response;
-                },function(response){
-                    console.log(response);
-                    return response;
-                });
-                return promise;
-            },
-            updatePosition:function(params){
-                var deferred=$q.defer();
-                var path=SERVICE_URL+'/setting/tree/position/update?';
-                var promise=$http.put( path+ params).then(function(response){
-                    return response;
-                },function(response){
-                    console.log(response);
-                    return response;
-                });
-                return promise;
-            },
-            addPosition:function(params){
-                var deferred=$q.defer();
-                var path=SERVICE_URL+'/setting/tree/position/add';
-                var promise=$http.post(path, params).then(function(response){
-                    return response;
-                },function(response){
-                    console.log(response);
-                    return response;
-                });
-                return promise;
-            },
-            deletePosition:function(params){
-                var deferred=$q.defer();
-                var path=SERVICE_URL+'/setting/tree/position/delete?';
-                var promise=$http.delete( path+ params).then(function(response){
-                    return response;
-                },function(response){
-                    console.log(response);
-                    return response;
-                });
-                return promise;
-            }
+.service('SeetingtreeService',['$q','$http','SERVICE_URL','$localStorage',function($q,$http,SERVICE_URL,$localStorage){
+    return{
+        getTreeList:function(id){
+            var deferred=$q.defer();
+            var path=SERVICE_URL+'/setting/tree/getlist?parent=0&access_token='+$localStorage.token;
+            if (id) {
+                path+='';
+            };
+            var promise=$http.get(path).then(function(response){
+                console.log(response);
+                return response;
+            },function(response){
+                console.log(response);
+                return response;
+            });
+            return promise;
+        },
+        addTree:function(params){
+            var deferred=$q.defer();
+            var path=SERVICE_URL+'/setting/tree/add';
+            var promise=$http.post(path, params).then(function(response){
+                return response;
+            },function(response){
+                console.log(response);
+                return response;
+            });
+            return promise;
+        },
+        updateTree:function(params){
+            var deferred=$q.defer();
+            var path=SERVICE_URL+'/setting/tree/update?';
+            var promise=$http.put( path+ params).then(function(response){
+                return response;
+            },function(response){
+                console.log(response);
+                return response;
+            });
+            return promise;
+        },
+        deleteTree:function(params){
+            var deferred=$q.defer();
+            var path=SERVICE_URL+'/setting/tree/delete?';
+            var promise=$http.delete( path+ params).then(function(response){
+                return response;
+            },function(response){
+                console.log(response);
+                return response;
+            });
+            return promise;
+        },
+        getPositionList:function(id){
+            var deferred=$q.defer();
+            var params=$.param({
+                tree_id:  id,
+                access_token:  $localStorage.token
+            });
+            var path=SERVICE_URL+'/setting/tree/position/getlist?';
+            var promise=$http.get(path+params).then(function(response){
+                console.log(response);
+                return response;
+            },function(response){
+                console.log(response);
+                return response;
+            });
+            return promise;
+        },
+        updatePosition:function(params){
+            var deferred=$q.defer();
+            var path=SERVICE_URL+'/setting/tree/position/update?';
+            var promise=$http.put( path+ params).then(function(response){
+                return response;
+            },function(response){
+                console.log(response);
+                return response;
+            });
+            return promise;
+        },
+        addPosition:function(params){
+            var deferred=$q.defer();
+            var path=SERVICE_URL+'/setting/tree/position/add';
+            var promise=$http.post(path, params).then(function(response){
+                return response;
+            },function(response){
+                console.log(response);
+                return response;
+            });
+            return promise;
+        },
+        deletePosition:function(params){
+            var deferred=$q.defer();
+            var path=SERVICE_URL+'/setting/tree/position/delete?';
+            var promise=$http.delete( path+ params).then(function(response){
+                return response;
+            },function(response){
+                console.log(response);
+                return response;
+            });
+            return promise;
         }
-    }])
+    }
+}]).service('SettingdaimaService',['$q','$http','$localStorage','SERVICE_URL',function($q,$http,$localStorage,SERVICE_URL){
+    return{
+        getCatalogList:function(){
+            var deferred=$q.defer();
+            var params=$.param({
+                access_token:  $localStorage.token
+            });
+            var path=SERVICE_URL+'/setting/code/getCatalogList?';
+            var promise=$http.get(path+params).then(function(response){
+                return response;
+            },function(response){
+                console.log(response);
+                return response;
+            });
+            return promise;
+        },
+        getCodagetList:function(catalog_id){
+            var deferred=$q.defer();
+            var params=$.param({
+                catalog_id:catalog_id,
+                access_token:  $localStorage.token
+            });
+            var path=SERVICE_URL+'/setting/code/getList?';
+            var promise=$http.get(path+params).then(function(response){
+                return response;
+            },function(response){
+                console.log(response);
+                return response;
+            });
+            return promise;
+        },
+        addDaima:function(params){
+            var deferred=$q.defer();
+            var path=SERVICE_URL+'/setting/code/add';
+            var promise=$http.post(path, params).then(function(response){
+                return response;
+            },function(response){
+                console.log(response);
+                return response;
+            });
+            return promise;
+        },
+        deleteDaima:function(params){
+            var deferred=$q.defer();
+            var path=SERVICE_URL+'/setting/code/delete?';
+            var promise=$http.delete( path+ params).then(function(response){
+                return response;
+            },function(response){
+                console.log(response);
+                return response;
+            });
+            return promise;
+        },
+        updateDaima:function(params){
+            var deferred=$q.defer();
+            var path=SERVICE_URL+'/setting/code/update?';
+            var promise=$http.put( path+ params).then(function(response){
+                return response;
+            },function(response){
+                console.log(response);
+                return response;
+            });
+            return promise;
+        }
+
+    }
+}])

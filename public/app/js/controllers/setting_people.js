de75274859a43b90433d243530042f1d56382631
wanglj <wanglj@int-yt.com>
2016-03-11 17:48:23 +0800

    人员管理

--- public/app/js/controllers/setting_people.js
+++ public/app/js/controllers/setting_people.js
@@ -153,6 +153,46 @@ app.controller('SetPeopleCtrl',['$scope','$http','$modal','$log','$debounce','Ta
         }
     );
 
+    //时间控件
+    $scope.today = function() {
+        $scope.birthday = new Date();
+    };
+    $scope.today();
+
+    $scope.clear = function () {
+        $scope.birthday = null;
+    };
+
+    // Disable weekend selection
+    $scope.disabled = function(date, mode) {
+        return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+    };
+
+    $scope.toggleMin = function() {
+        $scope.minDate = $scope.minDate ? null : new Date();
+    };
+    $scope.toggleMin();
+
+    $scope.open = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+
+        $scope.opened = true;
+    };
+
+    $scope.dateOptions = {
+        formatYear: 'yy',
+        startingDay: 1,
+        class: 'datepicker'
+    };
+
+    $scope.initDate = new Date('2016-15-20');
+    $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+    $scope.format = $scope.formats[0];
+
+
+
+
     //添加职务
     $scope.addzhiwu=function(peopleid){
         var modalzwaddInstance = $modal.open({

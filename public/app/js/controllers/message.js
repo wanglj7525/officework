1e2f70118291f37cda0b3c7668c42e981fbba9ae
wanglj <wanglj@int-yt.com>
2016-03-14 13:13:53 +0800

    信息浏览

--- public/app/js/controllers/message.js
+++ public/app/js/controllers/message.js
@@ -13,76 +13,135 @@
       $modalInstance.dismiss('cancel');
     };
   }]);
-  app.controller('MessageController', [ '$scope', '$http', '$state','$timeout','$modal','$log','$localStorage','UIMessageService','messageservice',
-		function($scope, $http, $state, $timeout,$modal,$log,$localStorage,UIMessageService,messageservice) {
+  app.controller('MessageController', [ '$scope', '$http', '$state','$timeout','$modal','$log','$localStorage','UIMessageService','messageservice','searchservice','SettingpeopleService','SettingdaimaService',
+		function($scope, $http, $state, $timeout,$modal,$log,$localStorage,UIMessageService,messageservice,searchservice,SettingpeopleService,SettingdaimaService) {
 			$scope.treeselected=$localStorage.treeselect;
-			console.log("信息浏览左树："+$scope.treeselected);
 			$scope.$watch(function(){ return $localStorage.treeselect},function(newValue,oldValue){
+				if(newValue===oldValue) return;
 				$scope.treeselected=$localStorage.treeselect;
-				console.log("信息浏览左树变换："+$scope.treeselected);
 			});
 
-			////分页获取数据
-			//var getMessageImageList = function () {
-			//	var postData = $.param({
-			//		pageNo: $scope.paginationConf.currentPage,
-			//		pageSize: $scope.paginationConf.itemsPerPage
-			//	});
-			//	UIMessageService.getMessageList(postData).then(
-			//		function (res) {
-			//			console.log(res);
-			//			if(res.data.code==200){
-			//				$scope.paginationConf.totalItems = res.data.totalElements;
-			//				$scope.imgs = res.data.info;
-			//			}else{
-			//				alert(res.data.msg);
-			//			}
-            //
-			//		},
-			//		function (rej) {
-			//			console.log(rej);
-			//		}
-			//	)
-			//}
-			////配置分页基本参数
-			//$scope.paginationConf = {
-			//	currentPage: 1,
-			//	itemsPerPage: 10
-			//};
-			///***************************************************************
-			// 当页码和页面记录数发生变化时监控后台查询
-			// 如果把currentPage和itemsPerPage分开监控的话则会触发两次后台事件。
-			// ***************************************************************/
-			//$scope.$watch('paginationConf.currentPage + paginationConf.itemsPerPage', getMessageImageList);
+			//性别
+			SettingdaimaService.getCodagetList("GB2261").then(function(res){ $scope.sexlist=res.data.info.list;},function(rej){});
+			//职级
+			SettingdaimaService.getCodagetList("FJ09").then(function(res){ $scope.zhijilist=res.data.info.list;},function(rej){});
+			//政治面貌
+			SettingdaimaService.getCodagetList("GB4762").then(function(res){ $scope.zhengzhilist=res.data.info.list;},function(rej){});
+			//学历
+			SettingdaimaService.getCodagetList("GB4658").then(function(res){ $scope.xuelilist=res.data.info.list;},function(rej){});
 
-
-
-            $scope.itemsByPage=10;
-			//获取人员信息
-			messageservice.getData().then(
+			$scope.selectparam=[];
+			//职级
+			$scope.rank=[];
+			//性别
+			$scope.xingbie=[];
+			//政治面貌
+			$scope.politicalstatus=[];
+			//学历
+			$scope.edulevel=[];
+			//年龄
+			$scope.age=[];
+			$scope.search={};
+			$scope.status = {
+				open: false
+			};
+			searchservice.getData().then(
 				function (res) {
-					console.log(11);
-					$scope.imgs = res.data.info;
-                    //$scope.messagetabletab = res.data.info;
+					$scope.nianlinglist = res.data.info;
 				},
 				function (rej) {
 					console.log(rej);
 				}
 			);
-            $scope.messagetabletab = [
-                           { id:1,img:'/public/app/img/a0.jpg', name:'张三1' ,sex:"男",company:"福清市xxx、xxx信息1",nation:"汉",birthday:"196511",palce:"福清龙田",troops:"197712",party:"191212",education:"本科",school:"福建师范"},
-                           { id:2,img:'/public/app/img/a1.jpg', name:'张三2' ,sex:"男",company:"福清市xxx、xxx信息2",nation:"汉",birthday:"195411",palce:"福清龙田",troops:"197212",party:"197712",education:"本科",school:"福建师范"},
-                           { id:3,img:'/public/app/img/a2.jpg', name:'张三3' ,sex:"男",company:"福清市xxx、xxx信息3",nation:"汉",birthday:"195711",palce:"福清龙田",troops:"191212",party:"197712",education:"本科",school:"福建师范"},
-                           { id:4,img:'/public/app/img/a3.jpg', name:'张三4' ,sex:"女",company:"福清市xxx、xxx信息4",nation:"汉",birthday:"195311",palce:"福清龙田",troops:"197212",party:"197212",education:"本科",school:"福建师范"},
-                           { id:5,img:'/public/app/img/a4.jpg', name:'张三5' ,sex:"男",company:"福清市xxx、xxx信息5",nation:"汉",birthday:"195511",palce:"福清龙田",troops:"191212",party:"197712",education:"本科",school:"福建师范"},
-                           { id:6,img:'/public/app/img/a5.jpg', name:'张三6' ,sex:"男",company:"福清市xxx、xxx信息6",nation:"汉",birthday:"195011",palce:"福清龙田",troops:"197212",party:"197712",education:"本科",school:"福建师范"},
-                           { id:7,img:'/public/app/img/a6.jpg', name:'张三7' ,sex:"男",company:"福清市xxx、xxx信息7",nation:"汉",birthday:"197511",palce:"福清龙田",troops:"197712",party:"197212",education:"本科",school:"福建师范"},
-                           { id:8,img:'/public/app/img/a7.jpg', name:'张三8' ,sex:"男",company:"福清市xxx、xxx信息8",nation:"汉",birthday:"198511",palce:"福清龙田",troops:"191212",party:"171212",education:"本科",school:"福建师范"},
-                           { id:9,img:'/public/app/img/a8.jpg', name:'张三9' ,sex:"男",company:"福清市xxx、xxx信息",nation:"汉",birthday:"195411",palce:"福清龙田",troops:"197712",party:"197712",education:"本科",school:"福建师范"},
-                           { id:10,img:'/public/app/img/a9.jpg', name:'张三10',sex:"男",company:"福清市xxx、xxx信息",nation:"汉",birthday:"195511",palce:"福清龙田",troops:"197212",party:"191212",education:"本科",school:"福建师范" },
-                           { id:11,img:'/public/app/img/a0.jpg', name:'张三1' ,sex:"男",company:"福清市xxx、xxx信息",nation:"汉",birthday:"195511",palce:"福清龙田",troops:"197212",party:"197712",education:"本科",school:"福建师范"},
-                           { id:12,img:'/public/app/img/a1.jpg', name:'张三2' ,sex:"女",company:"福清市xxx、xxx信息",nation:"汉",birthday:"195511",palce:"福清龙田",troops:"197712",party:"197212",education:"本科",school:"福建师范"},
-                           {id:21,img:'/public/app/img/a10.jpg', name:'张三11' ,sex:"男",company:"福清市xxx、xxx信息",nation:"汉",birthday:"195111",palce:"福清龙田",troops:"191212",party:"197712",education:"本科",school:"福建师范"}];
+			$scope.searchPeople=function(){
+				console.log($scope.search.keywords);
+				var postData = $.param({
+					keyword:$scope.search.keywords,
+					rank:$scope.rank.join(","),
+					sex:$scope.xingbie.join(","),
+					political_status:$scope.politicalstatus.join(","),
+					edu_level:$scope.edulevel.join(","),
+					age:$scope.age.join(","),
+					pageNo: $scope.paginationConf.currentPage,
+					pageSize: $scope.paginationConf.itemsPerPage,
+					access_token:$localStorage.token
+				});
+				SettingpeopleService.getPeopleList(postData).then(
+					function (res) {
+						console.log(res);
+						if(res.data.code==200){
+							$scope.paginationConf.totalItems = res.data.info.allRow;
+							$scope.messagetabletab = res.data.info.list;
+						}else{
+							alert(res.data.msg);
+						}
+
+					},
+					function (rej) {
+						console.log(rej);
+					}
+				)
+			}
+			//分页获取数据
+			var getMessageImageList = function () {
+				var postData = $.param({
+					name:$scope.searchtext,
+					pageNo: $scope.paginationConf.currentPage,
+					pageSize: $scope.paginationConf.itemsPerPage,
+					access_token:$localStorage.token
+				});
+				SettingpeopleService.getPeopleList(postData).then(
+					function (res) {
+						console.log(res);
+						if(res.data.code==200){
+							$scope.paginationConf.totalItems = res.data.info.allRow;
+							$scope.messagetabletab = res.data.info.list;
+						}else{
+							alert(res.data.msg);
+						}
+
+					},
+					function (rej) {
+						console.log(rej);
+					}
+				)
+			}
+			//配置分页基本参数
+			$scope.paginationConf = {
+				currentPage: 1,
+				itemsPerPage: 10
+			};
+			$scope.$watch('paginationConf.currentPage + paginationConf.itemsPerPage', getMessageImageList);
+			$scope.$watch('treeselected', getMessageImageList);
+
+
+
+            //$scope.itemsByPage=10;
+			//获取人员信息
+			//messageservice.getData().then(
+			//	function (res) {
+			//		console.log(11);
+			//		$scope.imgs = res.data.info;
+             //       //$scope.messagetabletab = res.data.info;
+			//	},
+			//	function (rej) {
+			//		console.log(rej);
+			//	}
+			//);
+            //$scope.messagetabletab = [
+            //               { id:1,img:'/public/app/img/a0.jpg', name:'张三1' ,sex:"男",company:"福清市xxx、xxx信息1",nation:"汉",birthday:"196511",palce:"福清龙田",troops:"197712",party:"191212",education:"本科",school:"福建师范"},
+            //               { id:2,img:'/public/app/img/a1.jpg', name:'张三2' ,sex:"男",company:"福清市xxx、xxx信息2",nation:"汉",birthday:"195411",palce:"福清龙田",troops:"197212",party:"197712",education:"本科",school:"福建师范"},
+            //               { id:3,img:'/public/app/img/a2.jpg', name:'张三3' ,sex:"男",company:"福清市xxx、xxx信息3",nation:"汉",birthday:"195711",palce:"福清龙田",troops:"191212",party:"197712",education:"本科",school:"福建师范"},
+            //               { id:4,img:'/public/app/img/a3.jpg', name:'张三4' ,sex:"女",company:"福清市xxx、xxx信息4",nation:"汉",birthday:"195311",palce:"福清龙田",troops:"197212",party:"197212",education:"本科",school:"福建师范"},
+            //               { id:5,img:'/public/app/img/a4.jpg', name:'张三5' ,sex:"男",company:"福清市xxx、xxx信息5",nation:"汉",birthday:"195511",palce:"福清龙田",troops:"191212",party:"197712",education:"本科",school:"福建师范"},
+            //               { id:6,img:'/public/app/img/a5.jpg', name:'张三6' ,sex:"男",company:"福清市xxx、xxx信息6",nation:"汉",birthday:"195011",palce:"福清龙田",troops:"197212",party:"197712",education:"本科",school:"福建师范"},
+            //               { id:7,img:'/public/app/img/a6.jpg', name:'张三7' ,sex:"男",company:"福清市xxx、xxx信息7",nation:"汉",birthday:"197511",palce:"福清龙田",troops:"197712",party:"197212",education:"本科",school:"福建师范"},
+            //               { id:8,img:'/public/app/img/a7.jpg', name:'张三8' ,sex:"男",company:"福清市xxx、xxx信息8",nation:"汉",birthday:"198511",palce:"福清龙田",troops:"191212",party:"171212",education:"本科",school:"福建师范"},
+            //               { id:9,img:'/public/app/img/a8.jpg', name:'张三9' ,sex:"男",company:"福清市xxx、xxx信息",nation:"汉",birthday:"195411",palce:"福清龙田",troops:"197712",party:"197712",education:"本科",school:"福建师范"},
+            //               { id:10,img:'/public/app/img/a9.jpg', name:'张三10',sex:"男",company:"福清市xxx、xxx信息",nation:"汉",birthday:"195511",palce:"福清龙田",troops:"197212",party:"191212",education:"本科",school:"福建师范" },
+            //               { id:11,img:'/public/app/img/a0.jpg', name:'张三1' ,sex:"男",company:"福清市xxx、xxx信息",nation:"汉",birthday:"195511",palce:"福清龙田",troops:"197212",party:"197712",education:"本科",school:"福建师范"},
+            //               { id:12,img:'/public/app/img/a1.jpg', name:'张三2' ,sex:"女",company:"福清市xxx、xxx信息",nation:"汉",birthday:"195511",palce:"福清龙田",troops:"197712",party:"197212",education:"本科",school:"福建师范"},
+            //               {id:21,img:'/public/app/img/a10.jpg', name:'张三11' ,sex:"男",company:"福清市xxx、xxx信息",nation:"汉",birthday:"195111",palce:"福清龙田",troops:"191212",party:"197712",education:"本科",school:"福建师范"}];
 
             //默认显示的tab
 			//$scope.selectTab=true;
--- public/app/js/directives/appdirectives.js
+++ public/app/js/directives/appdirectives.js
@@ -51,11 +51,41 @@ angular.module('app')
             link: function (scope, element, attrs) {
                function noselected() {
                    scope.selectparam.splice(scope.selectparam.indexOf(attrs.selectparam),1);
+                   if(attrs.rank){
+                       scope.rank.splice(scope.rank.indexOf(attrs.rank),1);
+                   }
+                   if(attrs.xingbie){
+                       scope.xingbie.splice(scope.xingbie.indexOf(attrs.xingbie),1);
+                   }
+                   if(attrs.politicalstatus){
+                       scope.politicalstatus.splice(scope.politicalstatus.indexOf(attrs.politicalstatus),1);
+                   }
+                   if(attrs.edulevel){
+                       scope.edulevel.splice(scope.edulevel.indexOf(attrs.edulevel),1);
+                   }
+                   if(attrs.age){
+                       scope.age.splice(scope.age.indexOf(attrs.age),1);
+                   }
                     element.removeClass('btn-danger');
                     element.addClass('btn-default');
                 }
                 function doseleted() {
                     scope.selectparam.push(attrs.selectparam);
+                    if(attrs.rank){
+                        scope.rank.push(attrs.rank);
+                    }
+                    if(attrs.xingbie){
+                        scope.xingbie.push(attrs.xingbie);
+                    }
+                    if(attrs.politicalstatus){
+                        scope.politicalstatus.push(attrs.politicalstatus);
+                    }
+                    if(attrs.edulevel){
+                        scope.edulevel.push(attrs.edulevel);
+                    }
+                    if(attrs.age){
+                        scope.age.push(attrs.age);
+                    }
                     element.removeClass('btn-default');
                     element.addClass('btn-danger');
                 }

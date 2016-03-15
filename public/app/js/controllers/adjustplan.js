2c5be1d4e5e6db9d2b5133ee3ba663a7c0971861
wanglj <wanglj@int-yt.com>
2016-03-15 18:36:09 +0800

    调整方案 删除 使用接口

--- public/app/js/controllers/adjustplan.js
+++ public/app/js/controllers/adjustplan.js
@@ -32,6 +32,34 @@ app.controller('planController',[ '$scope', '$http', '$state','$timeout','UIAdju
 			);
 
 		}
+		$scope.deletaplan=function(plan){
+			UIAdjustplanService.delAdjustPlan(plan.id).then(
+				function(res){
+					if(res.data.code==200){
+						UIAdjustplanService.getAdjustplanList().then(
+							function (res) {
+								$scope.planlist = res.data.info;
+								$scope.currentname=$scope.planlist[0].name;
+								UIAdjustplanService.getAdjustplanDetail($scope.planlist[0].id).then(
+									function(res){
+										$scope.plandetail=res.data.info;
+									},
+									function(rej){
+										console.log(rej);
+									}
+								);
+							},
+							function (rej) {
+								console.log(rej);
+							}
+						);
+					}
+				},
+				function(rej){
+					console.log(rej);
+				}
+			);
+		}
 	}
 ]);
 //app.controller('plandetailcontroller',[ '$scope', '$http', '$state','$timeout','$stateParams','plandetailservice',
